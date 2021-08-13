d424d39bfc90a176bda97771a328503ff57fe44d
Maciej Szwarc <emesz@protonmail.com>
2021-08-13 15:53:44 +0200

    add dead simple permission test page

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,14 @@ import { Route } from 'wouter';
 import CustomerDetailsWidget from './pages/customer-details-widget/CustomerDetailsWidget';
 import './App.css';
 import MessageBoxWidget from './pages/messagebox-widget/MessageBoxWidget';
+import Permissions from './pages/permissions/Permissions';
 
 function App(): JSX.Element {
   return (
     <div className="App">
       <Route path="/pages/customer-details-widget" component={CustomerDetailsWidget} />
       <Route path="/pages/message-box-widget" component={MessageBoxWidget} />
-      <Route path="/pages/test1">test1</Route>
+      <Route path="/pages/permissions" component={Permissions} />
     </div>
   );
 }
