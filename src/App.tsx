ebeb25f2759ceae881fad02cd59c9ada0cd31df9
Maciej Szwarc <emesz@protonmail.com>
2021-08-13 10:20:15 +0200

    add example widget in message box allowing sending rich messages

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { Route } from 'wouter';
 import CustomerDetailsWidget from './pages/customer-details-widget/CustomerDetailsWidget';
 import './App.css';
+import MessageBoxWidget from './pages/messagebox-widget/MessageBoxWidget';
 
 function App(): JSX.Element {
   return (
     <div className="App">
       <Route path="/pages/customer-details-widget" component={CustomerDetailsWidget} />
+      <Route path="/pages/message-box-widget" component={MessageBoxWidget} />
       <Route path="/pages/test1">test1</Route>
     </div>
   );
