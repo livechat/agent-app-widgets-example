992c8b3378cc21302a1e74c410902526f80f8eca
Michal Paszowski <m.paszowski@livechatinc.com>
2021-08-16 15:42:33 +0200

    counter counter

--- src/components/counter-badge/CounterBadge.tsx
+++ src/components/counter-badge/CounterBadge.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { createFullscreenWidget, IFullscreenWidget } from '@livechat/agent-app-sdk';
-import { Button, Card } from '@livechat/design-system';
+import { Button, Card, NumericInput } from '@livechat/design-system';
 import '@livechat/design-system/dist/design-system.css';
 
 export default function CustomerDetailsWidget(): React.ReactElement {
@@ -20,10 +20,25 @@ export default function CustomerDetailsWidget(): React.ReactElement {
   return (
     <div>
       <Card title="Badge tester:">
-        Click to increment counter in the badge:
-        <Button style={{ margin: '0.8rem' }} onClick={() => setCounter(counter + 1)}>
-          Add
-        </Button>
+        <div>
+          Click to play with the counter in the badge:
+          <Button style={{ margin: '0.8rem' }} onClick={() => setCounter(counter + 1)}>
+            Add
+          </Button>
+          <Button onClick={() => setCounter(counter - 1)}>Substract</Button>
+          <Button style={{ margin: '0.8rem' }} onClick={() => setCounter(0)}>
+            Reset
+          </Button>
+        </div>
+        <div>
+          Or set it manually:{' '}
+          <NumericInput
+            width="60px"
+            min={1}
+            value={counter as unknown as string}
+            onChange={(value) => setCounter(value as unknown as number)}
+          />
+        </div>
       </Card>
     </div>
   );
