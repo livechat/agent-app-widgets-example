de15473e4ff180741a9289b73ebb60a7fc83076f
Michal Paszowski <m.paszowski@livechatinc.com>
2021-08-16 08:15:46 +0200

    Examples of chat details widget and message box. (#11)
    * init
    
    * init configuration for eslint and prettier
    
    * test3
    
    * initial husky and staged config
    
    * wouter support
    
    * mishap
    
    * customer details base widget
    
    * design system, data presentation
    
    * simplyfing code
    
    Co-authored-by: Maciej Szwarc <emesz@protonmail.com>

---
+++ src/pages/customer-details-widget/CustomerDetailsWidget.tsx
@@ -0,0 +1,64 @@
+import React, { useEffect, useState } from 'react';
+import { createDetailsWidget, ICustomerProfile, SectionComponentType } from '@livechat/agent-app-sdk';
+import { Card, Divider } from '@livechat/design-system';
+import '@livechat/design-system/dist/design-system.css';
+import ReactJson from 'react-json-view';
+
+export default function CustomerDetailsWidget(): React.ReactElement {
+  const [customerProfile, setCustomerProfile] = useState<ICustomerProfile | null>(null);
+  useEffect(() => {
+    createDetailsWidget().then((widget) => {
+      widget.on('customer_profile', (profile) => {
+        setCustomerProfile(profile);
+        widget.modifySection({
+          title: 'Example Customer Details',
+          components: [
+            {
+              type: SectionComponentType.LabelValue,
+              data: {
+                label: 'Customer id:',
+                value: profile.id,
+              },
+            },
+            {
+              type: SectionComponentType.Button,
+              data: {
+                id: 'go-to-app',
+                label: 'Open application in a tab to see more',
+                openApp: true,
+                primary: true,
+              },
+            },
+          ],
+        });
+      });
+    });
+  }, []);
+
+  const cardTitle = customerProfile?.name as string;
+  const placeholder = <p>Customer profile object has not been loaded</p>;
+
+  if (!customerProfile) {
+    return placeholder;
+  }
+
+  return (
+    <p>
+      <header className="AppHeader">Details</header>
+      <main>
+        <Card title={cardTitle}>
+          <div>
+            <p>Customer profile object contains thread id and chat id:</p>
+            <ReactJson src={customerProfile.chat} collapsed />
+            <Divider />
+            <p>Geolocation data of the customer is also available:</p>
+            <ReactJson src={customerProfile.geolocation} collapsed />
+            <Divider />
+            <p>Full contents of Customer profile object:</p>
+            <ReactJson src={customerProfile} collapsed />
+          </div>
+        </Card>
+      </main>
+    </p>
+  );
+}
